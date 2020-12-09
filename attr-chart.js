3594663e68e3e784d772527deb80f5f50dcdf525
Guillapre <guillapre@gmail.com>
2020-12-09 07:19:20 -0300

    Brabo

--- attr-chart.js
+++ attr-chart.js
@@ -144,38 +144,7 @@ var attrs_desc = {
     }
 }
 function get_attr_range(value) {
-    if (value <= 1) {
-        return 1
-    }
-    else if (value <= 3) {
-        return 2
-    }
-    else if (value <= 5) {
-        return 3
-    }
-    else if (value <= 7) {
-        return 4
-    }
-    else if (value <= 9) {
-        return 5
-    }
-    else if (value <= 11) {
-        return 6
-    }
-    else if (value <= 13) {
-        return 7
-    }
-    else if (value <= 15) {
-        return 8
-    }
-    else if (value <= 17) {
-        return 9
-    }
-    else if (value <= 19) {
-        return 10
-    } else {
-        return 11
-    }
+    return ((value < 20) ? Math.round(parseInt(value)/2) : 11)
 }
 function get_modifier(value) {
     return ((value <= 9) ? Math.round((value-11)/2) : "+" + Math.round((value-11)/2))
